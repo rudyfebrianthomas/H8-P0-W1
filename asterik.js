3c7ef896f1589ef32cbe8380c56edb2de168c1ca
Rudy Febrianthomas <rudyfebrian6@gmail.com>
2019-03-08 09:17:21 +0700

    Tugas week 1

---
+++ asterik.js
@@ -0,0 +1,26 @@
+//Nomor 1
+console.log('Nomor 1');
+var row1=5;
+for (row1;row1>0;row1--){
+    console.log('*');
+}
+console.log('');
+//Nomor 2
+console.log('Nomor 2');
+var row2=5;
+for (row2;row2>0;row2--){
+  var b='';
+  for(var j=0;j<5;j++){
+    b+='*';
+  }
+  console.log(b);
+}
+console.log('');
+//Nomor 3
+console.log('Nomor 3');
+var row3=5;
+var b='';
+for (row3;row3>0;row3--){
+    b+='*';
+    console.log(b);
+}
